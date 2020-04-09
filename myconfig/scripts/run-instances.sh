df4f742f1cb6d29f87169abfb33df9658dd47a58
simshadows <contact@simshadows.com>
2020-04-09 10:27:44 +1000

    Added a kill script.

--- myconfig/scripts/run-instances.sh
+++ myconfig/scripts/run-instances.sh
@@ -11,10 +11,10 @@ sudo systemctl restart nginx php7.3-fpm
 # Set up tmux
 tmux new -ds 'rtorrent'
 tmux neww -t 'rtorrent' sudo -u rtorrent bash /mnt/drive11/rtorrent/instance-public0/instance-scripts/run.sh
-#tmux neww -t 'rtorrent' sudo -u rtorrent bash /mnt/drive0/rtorrent/instance-red0/instance-scripts/run.sh
-#tmux neww -t 'rtorrent' sudo -u rtorrent bash /mnt/drive0/rtorrent/instance-red0b/instance-scripts/run.sh
-#tmux neww -t 'rtorrent' sudo -u rtorrent bash /mnt/drive0/rtorrent/instance-red1/instance-scripts/run.sh
-#tmux neww -t 'rtorrent' sudo -u rtorrent bash /mnt/drive3/rtorrent/instance-red2/instance-scripts/run.sh
-#tmux neww -t 'rtorrent' sudo -u rtorrent bash /mnt/drive0/rtorrent/instance-nw0/instance-scripts/run.sh
-#tmux neww -t 'rtorrent' sudo -u rtorrent bash /mnt/drive0/rtorrent/instance-nw1/instance-scripts/run.sh
-#tmux neww -t 'rtorrent' sudo -u rtorrent bash /mnt/drive3/rtorrent/instance-jpopsuki0/instance-scripts/run.sh
+tmux neww -t 'rtorrent' sudo -u rtorrent bash /mnt/drive0/rtorrent/instance-red0/instance-scripts/run.sh
+tmux neww -t 'rtorrent' sudo -u rtorrent bash /mnt/drive0/rtorrent/instance-red0b/instance-scripts/run.sh
+tmux neww -t 'rtorrent' sudo -u rtorrent bash /mnt/drive0/rtorrent/instance-red1/instance-scripts/run.sh
+tmux neww -t 'rtorrent' sudo -u rtorrent bash /mnt/drive3/rtorrent/instance-red2/instance-scripts/run.sh
+tmux neww -t 'rtorrent' sudo -u rtorrent bash /mnt/drive25/rtorrent/instance-nw0/instance-scripts/run.sh
+tmux neww -t 'rtorrent' sudo -u rtorrent bash /mnt/drive25/rtorrent/instance-nw1/instance-scripts/run.sh
+tmux neww -t 'rtorrent' sudo -u rtorrent bash /mnt/drive3/rtorrent/instance-jpopsuki0/instance-scripts/run.sh
